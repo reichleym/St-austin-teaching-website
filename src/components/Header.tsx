0138272ef2d5c1a2fd88a2ac3d39b695e64219f5
reichleym <reichleym@yahoo.com>
2026-03-31 15:34:17 +0530

    Link tag replaced

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -48,9 +48,9 @@ export default function Header() {
                 <div className="flex justify-between items-center">
                     {/* Logo - Left Side */}
                     <div className="flex-shrink-0">
-                        <a href="/" className="">
+                        <Link href="/" className="">
                             <img src="/austin-logo.png" width={210} alt="Austin Logo" />
-                        </a>
+                        </Link>
                     </div>
 
 
@@ -59,13 +59,13 @@ export default function Header() {
                         <div className="hidden lg:block">
                             <div className="ml-10 flex space-x-5">
                                 {menuItems.map((item) => (
-                                    <a
+                                    <Link
                                         key={item.label}
                                         href={item.href}
                                         className="text-gray-700 hover:text-blue-600 transition-colors duration-200 font-medium"
                                     >
                                         {item.label}
-                                    </a>
+                                    </Link>
                                 ))}
                             </div>
                         </div>
@@ -102,13 +102,13 @@ export default function Header() {
                     <div className="lg:hidden pb-4">
                         <div className="flex flex-col space-y-2">
                             {menuItems.map((item) => (
-                                <a
+                                <Link
                                     key={item.label}
                                     href={item.href}
                                     className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-blue-600 hover:bg-gray-100 transition-colors duration-200"
                                 >
                                     {item.label}
-                                </a>
+                                </Link>
                             ))}
                         </div>
                         <div className="mt-4 flex flex-col space-y-3 border-t pt-4">
